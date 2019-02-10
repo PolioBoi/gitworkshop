1dd8898a709ffa14d69965ef7febb0e32a6baab8
Popippa <47263301+Popippa@users.noreply.github.com>
2019-02-10 21:48:17 +0800

    Add files via upload
    These are for the designchallenge package

---
+++ DesignChallenge1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designchallenge1;
+
+
+/**
+ *
+ * @author fredd
+ * Christian Alcala & Joshua Nicholas
+ * People who do codes wrong
+ */
+
+public class DesignChallenge1 {
+
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    	calendarView view = new calendarView();
+    	calendarModel model = new calendarModel();
+    	
+        CalendarProgram cp = new CalendarProgram(view, model);
+        cp.run();
+    }
+}
